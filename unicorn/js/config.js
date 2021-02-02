4122bf369e9cc8b202ef03333f858ae216a7a458
JI PENGXIANG <jipx@sp.edu.sg>
2021-02-02 16:09:14 +0800

    Add files via upload

---
+++ unicorn/js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_EsjXM66LX', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1f0u0848nns76p1h4b773c0ku4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://sixiem8wi0.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
